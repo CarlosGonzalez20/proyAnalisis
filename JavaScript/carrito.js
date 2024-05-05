48dd36f0fb116e40bff31a8e6703eea840b7a3e6
CarlosGonzalez20 <ll3224645@gmail.com>
2024-05-05 14:28:03 -0600

    adding invoice pop-up window, with a button to download in PDF (the js script needs to be added to save it)

--- JavaScript/carrito.js
+++ JavaScript/carrito.js
@@ -151,3 +151,17 @@ window.onload = function () {
   SumarPrecioTotal();
   ContarElementosCarrito();
 };
+
+document.getElementById('openModal').addEventListener('click', function() {
+  document.getElementById('modal').style.display = 'block';
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+  document.getElementById('modal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target == document.getElementById('modal')) {
+      document.getElementById('modal').style.display = 'none';
+  }
+});
